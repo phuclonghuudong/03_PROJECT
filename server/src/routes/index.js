8cc4b65084de8d17a72559f60429a1ffac376234
phuclonghuudong <phucpham.241099@gmail.com>
2024-09-14 11:59:37 +0700

    02 setup connect server

---
+++ server/src/routes/index.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const routerAPI = express.Router();
+
+module.exports = routerAPI;
---
+++ server/src/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express"); //commonjs
+const routerAPI = require("./routes/");
+const connection = require("./config/database");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// config cors
+app.use(cors());
+
+//config req.body
+app.use(express.json()); // for json
+app.use(express.urlencoded({ extended: true })); // for form data
+
+//khai báo route
+app.use("/v1/api/", routerAPI);
+
+(async () => {
+  try {
+    //using mongoose
+    await connection();
+
+    app.listen(port, () => {
+      console.log(`Backend Nodejs App listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(">>> Error connect to DB: ", error);
+  }
+})();
